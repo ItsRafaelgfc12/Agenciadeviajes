33fc9efb387387b3a1255cfe0efd31870cb7bc2c
ItsRafaelgfc12 <rafaeluzielf@gmail.com>
2024-12-08 09:41:18 -0500

    Cambios min

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
+import Content from './Components/Contenido';
 
 function App() {
   return (
     <Header>
       
     </Header>
+
   );
 }
 
